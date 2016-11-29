83f149e090a615c922a8a5b4dde9f5f9a5d48677
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2016-11-28 19:49:13 -0500

    Got rid of repeating code

--- sketch_nov27a/sketch_nov27a.ino
+++ sketch_nov27a/sketch_nov27a.ino
@@ -1,3 +1,5 @@
+#include "../Utilities/records.h"
+
 //Count pulses from opto encoder on carousel until the correct record is reached
 #define controlPulse 3
 //Pulse to stop carosel spinning
@@ -13,15 +15,7 @@
 #define controlSide 8
 //High when a record is playing
 #define pin 9
-//This is a datatype that can be either "A" or "B".
-enum side { A, B };
-typedef enum side side;
-//This is a datatype representing a record.
-//It has both a side and a record number.
-typedef struct {
-    side side;
-    int num;
-} record;
+
 int Home = 0;
 int incorrectSelection = 0;//goes to one if selection is incorrect
 int pulseCount = 0;//Amount of pulses on the pulse pin in one selection cycle
@@ -31,36 +25,8 @@ int lastState = 0;//Last state of pulse pin
 int errorValue = 0;//used to detect if there has been a mechanical error
 //Pin Variables
 
-
 void recordSelect(int id){
-   /**
-     * This function takes in a number assuming that:
-     * * It has three digits.
-     * * Its hundreds digit is 1 or 2.
-     * * Its tens digit is 0-9.
-     * * Its ones digit is 0-7.
-     * It then returns a record corresponding to that id number.
-    */
-    //First, compute the digits in id:
-    int digits[3];
-    for (int i = 0; i < 3; i++) {
-        //i % 10 always represents the ones digit of a number:
-        digits[i] = id % 10;
-        //Now, divide i by 10 to get rid of the last digit:
-        id /= 10;
-        /* This means that the next digit we add will be the digit right before the one we just added.
-           Thus, digits[0] is the ones digit,
-                 digits[1] is the tens digit,
-             and digits[2] is the hundreds digit. */
-    }
-    
-    record recordFromId;
-    //This sets the side to A if the hundreds digit is 1 and B otherwise.
-    recordFromId.side = (digits[2] == 1) ? A : B;
-    //This sets the record number to the ones digit times 10 plus the tens digit.
-    recordFromId.num = digits[0]*10+digits[1]+1;
-    Serial.print(recordFromId.side);
-    Serial.print(recordFromId.num);
+  record recordFromId = makeRecord(id);
   digitalWrite(controlStartSpin, HIGH);
   delay(250);
   if (((recordFromId.side == 0) && (digitalRead(controlSide) == HIGH)) || ((recordFromId.side == 1) && (digitalRead(controlSide) == LOW))){
